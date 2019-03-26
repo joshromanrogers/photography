b74f79b326a2b034bf5363a81dd0a201f81b3425
Josh Rogers <joshromanrogers@gmail.com>
2019-03-26 22:08:10 +0000

    home links and photo view on hover

---
+++ js/app.js
@@ -0,0 +1,37 @@
+let music = document.querySelector('.music-img');
+let travel = document.querySelector('.travel-img');
+let art = document.querySelector('.art-img');
+let misc = document.querySelector('.misc-img');
+
+let showMusic = () => {
+    music.style.display = "block";
+}
+
+let hideMusic = () => {
+    music.style.display = "none";
+}
+
+let showTravel = () => {
+    travel.style.display = "block";
+}
+
+let hideTravel = () => {
+    travel.style.display = "none";
+}
+
+let showArt = () => {
+    art.style.display = "block";
+}
+
+let hideArt = () => {
+    art.style.display = "none";
+}
+
+let showMisc = () => {
+    misc.style.display = "block";
+}
+
+let hideMisc = () => {
+    misc.style.display = "none";
+}
+       
