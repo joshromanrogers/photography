1171dd136417e4c7ac3c74a1517ee990e5374e91
Josh Rogers <joshromanrogers@gmail.com>
2019-03-27 16:46:18 +0000

    sliding names added to top + bottom

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+// slider function
+// (min number, max number, time interval between executing function, query selector)
+
+let slider = (min, max, speed, query) => {
+
+    let slider = 0;
+    let inc = -1;
+    let stripName = document.querySelector(`.${query}`);
+
+
+    // count every second
+    setInterval(() => {
+        if (slider == max) inc = -1; // once we reach max, start removing from slider
+        if (slider == min) inc = 1; // once we reach min, start adding to slider
+        slider += inc; // increment
+        stripName.style.transform = `translate3d(${slider}px, 0px, 0px)`;
+    }, speed);
+
+}
+
+slider(-100, 100, 80, 'movers');
+slider(-100, 100, 80, 'movers2');
