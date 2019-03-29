b76eebf1eac22a88c47f204c40522a2f416e6d0a
Josh Rogers <joshromanrogers@gmail.com>
2019-03-29 12:27:22 +0000

    lots of new files, zZoom up and working

--- js/scrollY.js
+++ js/scrollY.js
@@ -1,8 +1,10 @@
 const [redY, greenY, blueY] = [69, 111, 225];
 
 window.addEventListener('scroll', () => {
-  let y = 1 + (window.scrollY || window.pageYOffset) / 150
+  let y = 1 + (window.scrollY || window.pageYOffset) / 300
   y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
   const [rY, gY, bY] = [redY/y, greenY/y, blueY/y].map(Math.round)
  body.style.backgroundColor = `rgb(${rY}, ${gY}, ${bY})`
 })
+
+window.addEventListener()
