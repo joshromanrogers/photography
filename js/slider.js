edc9eeffd4d9e42eaf983b5866e021ffcc9bddcc
Josh Rogers <joshromanrogers@gmail.com>
2019-04-03 13:16:14 +0200

    sass overhaul, clear up and css files removal (few things to fix up)

--- js/slider.js
+++ js/slider.js
@@ -3,25 +3,25 @@
 
 let slider = (min, max, speed, query) => {
 
-    let slider = 0;
-    let inc = -1;
-    let stripName = document.querySelector(`.${query}`);
+	let slider = 0;
+	let inc = -1;
+	let stripName = document.querySelector(`.${query}`);
 
 
-    // count every second
-    setInterval(() => {
-        if (slider == max) inc = -1; // once we reach max, start removing from slider
-        if (slider == min) inc = 1; // once we reach min, start adding to slider
-        slider += inc; // increment
-        stripName.style.transform = `translate3d(${slider}px, 0px, 0px)`;
-    }, speed);
+	// count every second
+	setInterval(() => {
+		if (slider == max) inc = -1; // once we reach max, start removing from slider
+		if (slider == min) inc = 1; // once we reach min, start adding to slider
+		slider += inc; // increment
+		stripName.style.transform = `translate3d(${slider}px, 0px, 0px)`;
+	}, speed);
 
-}
+};
 
-slider(-150, 50, 80, 'movers');
-slider(-150, 50, 80, 'movers2');
-slider(-150, 50, 80, 'movers3');
-slider(-150, 50, 80, 'movers4');
-slider(-150, 50, 80, 'moversIndex');
+slider(-150, 50, 80, "movers");
+slider(-150, 50, 80, "movers2");
+slider(-150, 50, 80, "movers3");
+slider(-150, 50, 80, "movers4");
+slider(-150, 50, 80, "moversIndex");
 
 
